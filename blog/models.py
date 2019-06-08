14abe813129d39fee44944d98ad19d381f4d1e40
vmihda <vitalik.migda@gmail.com>
2019-06-08 15:54:18 +0300

    change migrate

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse("post_detail_url", kwargs={"slug": self.slug})
